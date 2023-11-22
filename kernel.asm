269444def3910bef5895ba430a16a555a6bfe94b
Matheus Augusto <62677004+MatheusMnt@users.noreply.github.com>
2023-11-22 15:14:05 -0300

    ajuste

--- kernel.asm
+++ kernel.asm
@@ -5,7 +5,7 @@ ORG 0x7C00
 BITS 16
     jmp start
 
-msg: db "aeiouVwxyZ", 0x0D, 0x0A, 0
+msg: db "Centro de informatica", 0x0D, 0x0A, 0
 
 start:
 
@@ -15,12 +15,47 @@ start:
     mov ss, ax
 
     mov si, msg
-    ;mov ax, 0
+    mov ax, 0
+    mov cx, 0
     call count_vogal
 
 end:
     jmp $ ; halt
 
+
+print_string:
+.loop:
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0E
+    int 0x10
+    jmp .loop
+.done:
+    ret
+
+print_number:
+    mov bx, 10
+    dec cx ;ele conta uma interação a mais 
+    mov ax, cx
+    mov cx, 0
+.loop1:
+    mov dx, 0
+    div bx
+    ; resposta vai ta no ax, resto no dx
+    add dx, 48
+    push dx
+    inc cx
+    cmp ax, 0
+    jne .loop1
+.loop2:
+    pop ax
+    mov ah, 0x0E
+    int 0x10
+    loop .loop2
+.done:
+    ret
+ 
 get_vogal:
     lodsb
     cmp al, 'a'
@@ -49,43 +84,8 @@ get_vogal:
 
 count_vogal:
    inc cx
-   call get_vogal
+   call get_vogal 
 
-print_string:
-.loop:
-    lodsb
-    or al, al
-    jz .done
-    mov ah, 0x0E
-    int 0x10
-    jmp .loop
-.done:
-    ret
-
-print_number:
-    mov bx, 10
-    mov ax, cx
-    mov cx, 0
-.loop1:
-    mov dx, 0
-    div bx
-    ; resposta vai ta no ax, resto no dx
-    add dx, 48
-    push dx
-    inc cx
-    cmp ax, 0
-    jne .loop1
-.loop2:
-    pop ax
-    mov ah, 0x0E
-    int 0x10
-    loop .loop2
-.done:
-    ret
- 
- 
-
- 
 ; assinatura de boot
 
     times 510-($-$$) db 0
